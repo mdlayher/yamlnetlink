ecb8941d205336e5aa0cbf25706e60033d298824
Matt Layher <mdlayher@gmail.com>
2022-09-16 07:58:18 -0400

    testdata: prep for multiple generated packages
    Signed-off-by: Matt Layher <mdlayher@gmail.com>

--- generate_test.go
+++ generate_test.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"os"
 	"os/exec"
+	"path/filepath"
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
@@ -11,29 +12,8 @@ import (
 	"golang.org/x/exp/slices"
 )
 
-func TestGenerate(t *testing.T) {
-	// Generate a nlctrl client in another package which we can compile, run,
-	// and verify it works.
-	code, err := yamlnetlink.Generate(nlctrl(), &yamlnetlink.Config{Package: "main"})
-	if err != nil {
-		t.Fatalf("failed to generate code: %v", err)
-	}
-
-	_ = os.Remove("testdata/nlctrl.go")
-
-	if err := os.WriteFile("testdata/nlctrl.go", code, 0o644); err != nil {
-		t.Fatalf("failed to write generated code: %v", err)
-	}
-
-	if err := os.Chdir("./testdata"); err != nil {
-		t.Fatalf("failed to change directory: %v", err)
-	}
-
-	// The output is a known JSON format we can compare.
-	out, err := exec.Command("go", "run", ".").CombinedOutput()
-	if err != nil {
-		t.Fatalf("failed to execute code: %v\noutput: %q", err, out)
-	}
+func TestGenerateNlctrl(t *testing.T) {
+	out := generate(t, "nlctrl")
 
 	type family struct {
 		ID   uint16 `json:"FamilyId"`
@@ -60,4 +40,59 @@ func TestGenerate(t *testing.T) {
 	if !slices.Contains(got.Families, "nlctrl") {
 		t.Fatalf("did not find nlctrl: %v", got.Families)
 	}
+}
+
+// generate generates and executes Go code for the specified family using the
+// family's directory under testdata.
+func generate(t *testing.T, family string) []byte {
+	t.Helper()
+
+	// Example: ./testdata/nlctrl/nlctrl
+	path := filepath.Join("testdata", family, family)
+	f, err := os.Open(path + ".yaml")
+	if err != nil {
+		t.Fatalf("failed to open file: %v", err)
+	}
+	defer f.Close()
+
+	spec, err := yamlnetlink.Parse(f)
+	if err != nil {
+		t.Fatalf("failed to parse spec: %v", err)
+	}
+	_ = f.Close()
+
+	// Generate a client in another package which we can compile, run, and
+	// verify it works.
+	code, err := yamlnetlink.Generate(spec, &yamlnetlink.Config{Package: "main"})
+	if err != nil {
+		t.Fatalf("failed to generate code: %v", err)
+	}
+
+	_ = os.Remove(path + ".go")
+	if err := os.WriteFile(path+".go", code, 0o644); err != nil {
+		t.Fatalf("failed to write generated code: %v", err)
+	}
+
+	pwd, err := os.Getwd()
+	if err != nil {
+		t.Fatalf("failed to get working directory: %v", err)
+	}
+
+	if err := os.Chdir(filepath.Dir(path)); err != nil {
+		t.Fatalf("failed to change working directory: %v", err)
+	}
+
+	defer func() {
+		if err := os.Chdir(pwd); err != nil {
+			t.Fatalf("failed to restore working directory: %v", err)
+		}
+	}()
+
+	// Execute the code and pass stdout back to the caller.
+	out, err := exec.Command("go", "run", ".").CombinedOutput()
+	if err != nil {
+		t.Fatalf("failed to execute code: %v\noutput: %q", err, out)
+	}
+
+	return out
 }
